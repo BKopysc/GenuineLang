6ff7f5eaf1a6a2d8759a965108f47580edbc6455
Bartlomiej Kopysc <bkopysc@wp.pl>
2023-05-31 22:38:38 +0200

    Basic grammar features v1

--- LLVMGenerator.java
+++ LLVMGenerator.java
@@ -3,5 +3,33 @@
  * It will be called by the main class.
  */
 
-public class LLVMGenerator {
+// enum VarType{
+//     INT,
+//     REAL,
+//     UNKNOWN
+// }
+
+class LLVMGenerator {
+    
+    static String header_text = "";
+    static String main_text = "";
+    static int reg = 1;
+
+    static void declare_int(String id){
+      main_text += "%"+id+" = alloca i32\n";
+   }
+
+   static void declare_real(String id){
+        main_text += "%"+id+" = alloca double\n";
+    }
+
+      static void assign_int(String id, String value){
+      main_text += "store i32 "+value+", i32* %"+id+"\n";
+   }
+
+    static void assign_real(String id, String value){
+      main_text += "store double "+value+", double* %"+id+"\n";
+   }
+
+   
 }
