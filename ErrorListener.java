d61e4d8ea13a8c8814a3b658081db86f9e68b051
folejniczak <folejn@gmail.com>
2023-05-31 22:43:59 +0200

    error handling

---
+++ ErrorListener.java
@@ -0,0 +1,17 @@
+import org.antlr.v4.runtime.;
+import org.antlr.v4.runtime.tree.;
+
+public class ErrorListener extends BaseErrorListener {
+    public static final ErrorListener INSTANCE = new ErrorListener();
+
+    public ErrorListener() {
+        super();
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+        String errorMsg = String.format("Parsing error, line %s:%s :: [%s]", line, charPositionInLine, msg);
+        System.err.println(errorMsg);
+        System.exit(1);
+    }
+}
